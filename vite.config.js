7a039e8e12e2e156f458eec40e05c3fcd6e403ae
RaulTindogan <tindoganraul@gmail.com>
2024-02-11 19:44:13 +0800

    update 404 page

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/SHINU/",
+  base: '/SHINU/',
   plugins: [react()],
   build: { chunkSizeWarningLimit: 1600, }
 })
